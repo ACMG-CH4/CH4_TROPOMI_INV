8c167a528933823a4ecd3ac090063dec2c4fea68
Daniel Varon <dvaron@boslogin02.rc.fas.harvard.edu>
2020-08-07 14:14:07 -0400

    Comment and clean Step4

--- Step4_inversion_pixel/Step1_invert.py
+++ Step4_inversion_pixel/Step1_invert.py
@@ -7,109 +7,161 @@ import xarray as xr
 import pickle
 import os
 
-#----- define function -------
+# Notes:
+# ======
+# - Manual step: generate observational error data file "mean_error.nc"
+# - Inversion steps at the end are not clear. Need new variable names. Possible to rewrite that
+#   section in a more mathematically transparent way, or will that slow things down?
+
+# ==================================================================================================
+#
+#                                      Define functions
+#
+# ==================================================================================================
+
 def save_obj(obj, name ):
+""" Save something with Pickle. """
+
     with open(name , 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+
+# --------------------------------------------------------------------------------------------------
+
 def load_obj(name):
+""" Load something with Pickle. """
+
     with open( name, 'rb') as f:
         return pickle.load(f)
 
-def nearest_loc(loc0,table,tolerance=1):
-    temp=np.abs(table-loc0)
-    ind=temp.argmin()
-    if temp[ind]>=tolerance:
+
+# --------------------------------------------------------------------------------------------------
+
+def nearest_loc(loc_query, loc_grid, tolerance=1):
+""" Find the index of the nearest grid location to a query location, with some tolerance. """
+
+    distances = np.abs(loc_grid - loc_query)
+    ind = temp.argmin()
+    if distances[ind] >= tolerance:
         return np.nan
     else:
         return ind
 
-#---- read obs error data ---
-filename="/n/holyscratch01/jacob_lab/lshen/CH4/GEOS-Chem/Flexgrid/CPU_mexico_inversion/Step4_inversion_pixel/mean_error.nc"
-data=xr.open_dataset(filename)
-lon_GC=data['lon'].values
-lat_GC=data['lat'].values
-mean_error=data['error'].values
-mean_error=mean_error**2
-data.close()
-mean_error=np.einsum('ij->ji', mean_error)
 
-#-----------------------------
+# ==================================================================================================
+#
+#                                      Run the code
+#
+# ==================================================================================================
+
+# Configuration
+n_clust = 1199+8
+xlim = [-106.5625,-82.1875];
+ylim = [11,35]
+
+# Read observational error data
+filename = "/n/holyscratch01/jacob_lab/lshen/CH4/GEOS-Chem/Flexgrid/CPU_mexico_inversion/Step4_inversion_pixel/mean_error.nc"
+data = xr.open_dataset(filename)
+lon_GC = data['lon'].values
+lat_GC = data['lat'].values
+mean_error_std = data['error'].values
+mean_error = mean_error_std**2                  # Error variance from standard deviation
+mean_error = np.einsum('ij->ji', mean_error)
+data.close()
+
+# Read output data from Step1 (virtual TROPOMI column XCH4, Jacobian matrix)
 os.chdir("/n/holyscratch01/jacob_lab/lshen/CH4/GEOS-Chem/Flexgrid/CPU_mexico_inversion/data_converted/")
-files=glob.glob("*.pkl")
+files = glob.glob("*.pkl")
 files.sort()
 
-all_part1=np.zeros([1207,1207],dtype=float)
-all_part2=np.zeros([1207],dtype=float)
-xlim=[-101.875,-86.875];ylim=[13,30]
-xlim=[-107.8125,-80.9375];ylim=[10,36]
-xlim=[-106.5625,-82.1875];ylim=[11,35]
+# Initialize ________
+all_part1 = np.zeros([n_clust,n_clust], dtype=float)
+all_part2 = np.zeros([n_clust], dtype=float)
 
-for ifile in range(len(files)):
-    print(files[ifile])
-    met=load_obj(files[ifile])
-    if met['obs_GC'].shape[0]==0:
+# For each .pkl file from Step1:
+for fi in files:
+    
+    # Load TROPOMI/GEOS-Chem and Jacobian matrix data from the .pkl file
+    print(fi)
+    met = load_obj(fi)
+    # If there aren't any TROPOMI observations on this day, skip 
+    # [****Shouldn't we have no files for those cases anyway?]
+    if met['obs_GC'].shape[0] == 0:
         continue
-    
-    obs_GC=met['obs_GC']
-    ind=np.where( (obs_GC[:,2]>=xlim[0]) & (obs_GC[:,2]<=xlim[1]) & (obs_GC[:,3]>=ylim[0]) & (obs_GC[:,3]<=ylim[1]) )
-    if (len(ind[0])==0):
+    # Otherwise, grab the TROPOMI/GEOS-Chem data
+    obs_GC = met['obs_GC']
+    # Only consider data within latitude and longitude bounds
+    ind = np.where((obs_GC[:,2]>=xlim[0]) & (obs_GC[:,2]<=xlim[1]) & (obs_GC[:,3]>=ylim[0]) & (obs_GC[:,3]<=ylim[1]))
+    if (len(ind[0]) == 0):    # Skip if no data in bounds
         continue
+    obs_GC = obs_GC[ind[0],:] # TROPOMI and GEOS-Chem data within bounds
+    KK = met['KK'][ind[0],:]  # Jacobian entries for observations within bounds
+    NN = obs_GC.shape[0]      # Number of observations
 
-    obs_GC=obs_GC[ind[0],:]        
-    KK=met['KK'][ind[0],:]
-    NN=obs_GC.shape[0]
-
-    #now lower the sensitivity to BC by 50%
-    #KK[:,1199:]=KK[:,1199:]/2
+    # Now lower the sensitivity to BC by 50%
+    # [****Is this something we want to do? Delete these lines?]
+    #KK[:,1199:] = KK[:,1199:]/2
     
-    obs_error=np.zeros((NN,))    
-    deltaY=obs_GC[:,0]-obs_GC[:,1]
-    
+    # Initialize observation error matrix diagonal entries
+    obs_error = np.zeros((NN,))
+    # For each TROPOMI observation:
     for iNN in range(NN):
-        iGC=nearest_loc(obs_GC[iNN,2],lon_GC)
-        jGC=nearest_loc(obs_GC[iNN,3],lat_GC)        
-        obs_error[iNN]=mean_error[iGC,jGC]
-            
+        # Get the closest GC pixel by latitude and longitude
+        iGC = nearest_loc(obs_GC[iNN,2], lon_GC)
+        jGC = nearest_loc(obs_GC[iNN,3], lat_GC)
+        # Get the observational error for that location
+        obs_error[iNN] = mean_error[iGC,jGC]
+    
+    # Measurement-model mismatch: TROPOMI columns minus GEOS-Chem virtual TROPOMI columns 
+    deltaY = obs_GC[:,0] - obs_GC[:,1] # [ppb]
+    # If there are any nan's in the data, abort 
     if (np.any(np.isnan(deltaY)) or np.any(np.isnan(KK)) or np.any(np.isnan(obs_error))):
-        print('missing values', ifile, files[ifile])
+        print('missing values', fi)
         break
-        
-    KK_t=KK.transpose()
     
-    #----------    
-    KK_t2=np.zeros(KK_t.shape,dtype=float)
+    # [****What are these steps?]
+    # [****We're clearly building some terms for the inversion here]
+    KK_t = KK.transpose() 
+    KK_t2 = np.zeros(KK_t.shape, dtype=float)
     for k in range(KK_t.shape[1]):
-        KK_t2[:,k]=KK_t[:,k]/obs_error[k]        
+        KK_t2[:,k] = KK_t[:,k]/obs_error[k]        
 
-    part1=KK_t2@KK
-    part2=KK_t2@deltaY
-    
-    all_part1=all_part1+part1
-    all_part2=all_part2+part2
+    # [****What are part1 and part2?]
+    # [****What is happening here?]
+    part1 = KK_t2@KK
+    part2 = KK_t2@deltaY
+   
+    # Add part1 & part2 to sums 
+    all_part1 += part1
+    all_part2 += part2
         
-        
-emis_error=np.zeros(1207);emis_error.fill(0.5**2)
-inv_Sa=np.diag(1/emis_error)
-ratio=np.linalg.inv(all_part1+inv_Sa)@all_part2
-print(ratio.max(),ratio.mean(),ratio.min())
+# Prior covariance matrix
+emis_error = np.zeros(n_clust);
+emis_error.fill(0.5**2)          # 50% error, 1-sigma
+inv_Sa = np.diag(1/emis_error)   # Inverse of prior covariance matrix
 
-met={}
-met['all_part1']=all_part1
-met['all_part2']=all_part2
-met['ratio']=ratio
+# Solve for posterior scaling factors
+# [****Is that what "ratio" is?]
+ratio = np.linalg.inv(all_part1 + inv_Sa)@all_part2
 
-#======= save results ======
-outputname='/n/holyscratch01/jacob_lab/lshen/CH4/GEOS-Chem/Flexgrid/CPU_mexico_inversion/Step4_inversion_pixel/inversion_result.nc'
-dataset = Dataset(outputname,'w',format='NETCDF4_CLASSIC')
-nvar=dataset.createDimension('nvar', 1207)
+# Print some statistics
+print('Min:',ratio.min(),'Mean:',ratio.mean(),'Max',ratio.max())
 
+# Dictionary to store results
+# [****But we don't save it out? Delete these lines?]
+#met = {}
+#met['all_part1'] = all_part1
+#met['all_part2'] = all_part2
+#met['ratio'] = ratio
+
+# Save results
+outputname = '/n/holyscratch01/jacob_lab/lshen/CH4/GEOS-Chem/Flexgrid/CPU_mexico_inversion/Step4_inversion_pixel/inversion_result.nc'
+dataset = Dataset(outputname, 'w', format='NETCDF4_CLASSIC')
+nvar = dataset.createDimension('nvar', n_clust)
 nc_all_part1 = dataset.createVariable('all_part1', np.float32,('nvar','nvar'))
 nc_all_part2 = dataset.createVariable('all_part2', np.float32,('nvar'))
 nc_ratio = dataset.createVariable('ratio', np.float32,('nvar'))
-
-nc_all_part1[:,:]=all_part1
-nc_all_part2[:]=all_part2
-nc_ratio[:]=ratio
-
+nc_all_part1[:,:] = all_part1    # [****What is this?]
+nc_all_part2[:] = all_part2      # [****What is this?]
+nc_ratio[:] = ratio              # [****What is this?]
 dataset.close()
