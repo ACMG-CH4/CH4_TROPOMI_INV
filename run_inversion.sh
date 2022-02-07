0297dda3000b8893cc14f508b73cb77e38fb89ee
Daniel Varon <danieljordanvaron@gmail.com>
2022-02-07 15:44:02 -0500

    Add PrecomputedJacobian config setting to skip calc_sensi, setup_gc_cache, other Python steps when conducting sensitivity inversions or inversions with precomputed Jacobian. Also remove duplicated config settings in run_ch4_inversion.sh ; settings were being read from config.yml and also added into run_inversion.sh from setup script using sed.

--- run_inversion.sh
+++ run_inversion.sh
@@ -17,24 +17,22 @@ printf "\n=== PARSING CONFIG FILE ===\n"
 # Get configuration
 source ../../../setup_CH4/parse_yaml.sh
 eval $(parse_yaml ../../../setup_CH4/config.yml)
-# This defines $PriorError, $ObsError, and $Gamma (in addition to the other config entries)
+# This defines $StartDate, $EndDate, $nBufferClusters, $RunName, $isAWS
+# It also define $PriorError, $ObsError, $Gamma, $PrecomputedJacobian
 # Parsing the config file here facilitates generation of inversion ensembles
-# All that needs to be done is to edit the config file for $PriorError, $ObsError, and $Gamma, then re-run this script
+# All that needs to be done is to edit the config file for $PriorError, $ObsError, and $Gamma, 
+# make sure $PrecomputedJacobian is true, and then re-run this script (or run_ch4_inversion.sh)
 
 #=======================================================================
 # Configuration (these settings generated on initial setup)
 #=======================================================================
-StartDate={START}
-EndDate={END}
 LonMinInvDomain={LON_MIN}
 LonMaxInvDomain={LON_MAX}
 LatMinInvDomain={LAT_MIN}
 LatMaxInvDomain={LAT_MAX}
 nElements={STATE_VECTOR_ELEMENTS}
-nBufferClusters={BUFFER_CLUSTERS}
 MyPath={MY_PATH}
-RunName={RUN_NAME}
-isAWS={IS_AWS}
+Res={RES}
 SpinupDir="${MyPath}/${RunName}/spinup_run"
 JacobianRunsDir="${MyPath}/${RunName}/jacobian_runs"
 PosteriorRunDir="${MyPath}/${RunName}/posterior_run"
@@ -73,64 +71,75 @@ fi
 # Postprocess the SpeciesConc and LevelEdgeDiags files from GEOS-Chem
 #=======================================================================
 
-printf "Calling postproc_diags.py, FSS=$FirstSimSwitch\n"
-if "$FirstSimSwitch"; then
-    if [[ ! -d ${SpinupDir} ]]; then
-	printf "${SpinupDir} does not exist. Please fix SpinupDir or set FirstSimSwitch to False in run_inversion.sh.\n"
-	exit 1
-    fi
-    PrevDir=$SpinupDir
-else
-    PrevDir=%$PosteriorRunDir
-    if [[ ! -d ${PosteriorRunDir} ]]; then
-	printf "${PosteriorRunDir} does not exist. Please fix PosteriorRunDir in run_inversion.sh.\n"
-	exit 1
-    fi
-fi
-printf "  - Hour 0 for ${StartDate} will be obtained from ${PrevDir}\n"
+if ! "$PrecomputedJacobian"; then
 
-python postproc_diags.py $RunName $JacobianRunsDir $PrevDir $StartDate; wait
-printf "DONE -- postproc_diags.py\n\n"
+    printf "Calling postproc_diags.py, FSS=$FirstSimSwitch\n"
+    if "$FirstSimSwitch"; then
+        if [[ ! -d ${SpinupDir} ]]; then
+        printf "${SpinupDir} does not exist. Please fix SpinupDir or set FirstSimSwitch to False in run_inversion.sh.\n"
+        exit 1
+        fi
+        PrevDir=$SpinupDir
+    else
+        PrevDir=%$PosteriorRunDir
+        if [[ ! -d ${PosteriorRunDir} ]]; then
+        printf "${PosteriorRunDir} does not exist. Please fix PosteriorRunDir in run_inversion.sh.\n"
+        exit 1
+        fi
+    fi
+    printf "  - Hour 0 for ${StartDate} will be obtained from ${PrevDir}\n"
+
+    python postproc_diags.py $RunName $JacobianRunsDir $PrevDir $StartDate; wait
+    printf "DONE -- postproc_diags.py\n\n"
+
+fi
 
 #=======================================================================
 # Calculate GEOS-Chem sensitivities and save to Sensi directory
 #=======================================================================
 
-# 50% perturbation implied by PerturbValue (config.yml): 1.5 - 1 = 0.5
-Perturbation=0.5
+if ! "$PrecomputedJacobian"; then
 
-printf "Calling calc_sensi.py\n"
-python calc_sensi.py $nElements $Perturbation $StartDate $EndDate $JacobianRunsDir $RunName $SensiDir; wait
-printf "DONE -- calc_sensi.py\n\n"
+    # 50% perturbation
+    Perturbation=0.5
+
+    printf "Calling calc_sensi.py\n"
+    python calc_sensi.py $nElements $Perturbation $StartDate $EndDate $JacobianRunsDir $RunName $SensiDir; wait
+    printf "DONE -- calc_sensi.py\n\n"
+
+fi
 
 #=======================================================================
 # Setup GC data directory in workdir
 #=======================================================================
-GCsourcepth="${JacobianRunsDir}/${RunName}_0000/OutputDir"
 
-printf "Calling setup_gc_cache.py\n"
-python setup_gc_cache.py $StartDate $EndDate $GCsourcepth $GCDir; wait
-printf "DONE -- setup_gc_cache.py\n\n"
+if ! "$PrecomputedJacobian"; then
+
+    GCsourcepth="${JacobianRunsDir}/${RunName}_0000/OutputDir"
+
+    printf "Calling setup_gc_cache.py\n"
+    python setup_gc_cache.py $StartDate $EndDate $GCsourcepth $GCDir; wait
+    printf "DONE -- setup_gc_cache.py\n\n"
+
+fi
 
 #=======================================================================
 # Generate Jacobian matrix files 
 #=======================================================================
 
-printf "Calling jacobian.py\n"
-isPost="False"
-python jacobian.py $StartDate $EndDate $LonMinInvDomain $LonMaxInvDomain $LatMinInvDomain $LatMaxInvDomain $nElements $tropomiCache $isPost; wait
-printf " DONE -- jacobian.py\n\n"
+if ! "$PrecomputedJacobian"; then
+
+    printf "Calling jacobian.py\n"
+    isPost="False"
+    python jacobian.py $StartDate $EndDate $LonMinInvDomain $LonMaxInvDomain $LatMinInvDomain $LatMaxInvDomain $nElements $tropomiCache $isPost; wait
+    printf " DONE -- jacobian.py\n\n"
+
+fi
 
 #=======================================================================
 # Do inversion
 #=======================================================================
 
-# Set input values
-LonMinInvDomain={LON_MIN}
-LonMaxInvDomain={LON_MAX}
-LatMinInvDomain={LAT_MIN}
-LatMaxInvDomain={LAT_MAX}
-Res={RES}
 posteriorSF="./inversion_result.nc"
 
 printf "Calling invert.py\n"
